e4847f4322b3e383cc1f223765dab494bfb55cb5
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-08 01:24:12 +0530

    client start

---
+++ Netwrk_lab/FTP/client.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<string.h>
+#include<arpa/inet.h>
+
+void main()
+{
+    int socketId;
+    struct sockaddr_in serverAddress;
+    socketId=socket(AF_INET,);
+}
+
