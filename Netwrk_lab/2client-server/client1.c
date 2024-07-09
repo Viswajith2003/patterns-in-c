245a2f2d8dcf3bebc3a81f5fe4012aff6019907a
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-09 16:05:28 +0530

    as

--- Netwrk_lab/2client-server/client1.c
+++ Netwrk_lab/2client-server/client1.c
@@ -1,3 +1,4 @@
+
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<stdio.h>
@@ -17,7 +18,7 @@ void main()
     printf("socket creation successfull\n");
     memset(&serverAddress,0,sizeof(serverAddress));
     serverAddress.sin_family=AF_INET;
-    serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+    serverAddress.sin_addr.s_addr=inet_addr("127.0.0.1");
     serverAddress.sin_port=htons(8080);
     if(connect(socketId,(struct sockaddr *)&serverAddress,sizeof(serverAddress))!=0)
     {
@@ -39,11 +40,13 @@ void main()
             close(socketId);
             return;
         }
-        if(strcmp(buffer,"exit")==0)
-        {
-            printf("client exiting");
-            break;
-        }
+        // if(strcmp(buffer,"exit")==0)
+        // {
+        //     printf("client exiting");
+        //     break;
+        // }
+
+
         ssize_t Received=recv(socketId,buffer,sizeof(buffer),0);
         if(Received==-1)
         {
@@ -57,12 +60,78 @@ void main()
             printf("server disconnected");
             break;
         }
+        buffer[Received]="\0";
         printf("\nfrom server %s:",buffer);
-        if(strcmp(buffer,"exit")==0)
-        {
-            printf("exit cmd received, close the connection");
-            break;
-        }
+        // if(strcmp(buffer,"exit")==0)
+        // {
+        //     printf("exit cmd received, close the connection");
+        //     break;
+        // }
     }
     close(socketId);
 }
+
+
+
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// #include <stdio.h>
+// #include <unistd.h>
+// #include <arpa/inet.h>
+// #include <string.h>
+
+// void main() {
+//     int socketId;
+//     struct sockaddr_in serverAddress;
+//     socketId = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+//     if (socketId == -1) {
+//         printf("socket creation failed\n");
+//         return;
+//     }
+//     printf("socket creation successful\n");
+    
+//     memset(&serverAddress, 0, sizeof(serverAddress));
+//     serverAddress.sin_family = AF_INET;
+//     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // assuming server is on localhost
+//     serverAddress.sin_port = htons(8080);
+    
+//     if (connect(socketId, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0) {
+//         printf("connection failed\n");
+//         close(socketId);
+//         return;
+//     }
+//     printf("successfully connected\n");
+    
+//     char buffer[100];
+//     while (1) {
+//         printf("\nTo server: ");
+//         fgets(buffer, sizeof(buffer), stdin);
+//         buffer[strcspn(buffer, "\n")] = '\0'; // Remove the newline character
+        
+//         if (send(socketId, buffer, strlen(buffer) + 1, 0) == -1) {
+//             printf("send failed\n");
+//             close(socketId);
+//             return;
+//         }
+        
+//         ssize_t Received = recv(socketId, buffer, sizeof(buffer), 0);
+//         if (Received == -1) {
+//             printf("Receive failed\n");
+//             close(socketId);
+//             return;
+//         } else if (Received == 0) {
+//             printf("server disconnected\n");
+//             break;
+//         }
+//         buffer[Received] = '\0';
+//         printf("from server: %s\n", buffer);
+//     }
+    
+//     close(socketId);
+// }
+
+
+
+
+
+
--- Netwrk_lab/2client-server/server.c
+++ Netwrk_lab/2client-server/server.c
@@ -1,3 +1,4 @@
+
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<stdio.h>
@@ -43,7 +44,8 @@ void main()
         close(connId);
         return;
     }
-    printf("client accepted\n");
+    printf("client1 accepted\n");
+
 
     char buffer[100];
     while(1)
@@ -62,13 +64,13 @@ void main()
             break;
         }
         printf("\nfrom client %s:",buffer);
-        if(strcmp(buffer,"exit")==0)
-        {
-            printf("exit cmd received, close the connection");
-            break;
-        }
+        // if(strcmp(buffer,"exit")==0)
+        // {
+        //     printf("exit cmd received, close the connection");
+        //     break;
+        // }
 
-        printf("\nTo client :");
+        printf("\nTo client2 :");
         fgets(buffer,sizeof(buffer),stdin);
         buffer[strcspn(buffer,"\n")]!="\0";
         if(send(connId,buffer,strlen(buffer)+1,0)==-1)
@@ -78,12 +80,98 @@ void main()
             close(connId);
             return;
         }
-        if(strcmp(buffer,"exit")==0)
-        {
-            printf("server exit");
-            break;
-        }
+        // if(strcmp(buffer,"exit")==0)
+        // {
+        //     printf("server exit");
+        //     break;
+        // }
     }
     close(connId);
     close(socketId);
 }
+
+
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// #include <stdio.h>
+// #include <unistd.h>
+// #include <arpa/inet.h>
+// #include <string.h>
+
+// void main() {
+//     int socketId, connId1, connId2;
+//     struct sockaddr_in serverAddress, clientAddress;
+//     socketId = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+//     if (socketId == -1) {
+//         printf("socket creation failed\n");
+//         return;
+//     }
+//     printf("socket creation successful\n");
+    
+//     memset(&serverAddress, 0, sizeof(serverAddress));
+//     serverAddress.sin_family = AF_INET;
+//     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+//     serverAddress.sin_port = htons(8080);
+    
+//     if (bind(socketId, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0) {
+//         printf("bind unsuccessful\n");
+//         close(socketId);
+//         return;
+//     }
+//     printf("successfully bound\n");
+    
+//     if (listen(socketId, 5) == -1) {
+//         printf("Listen failed\n");
+//         close(socketId);
+//         return;
+//     }
+//     printf("Listening...\n");
+    
+//     socklen_t len = sizeof(clientAddress);
+//     connId1 = accept(socketId, (struct sockaddr *)&clientAddress, &len);
+//     if (connId1 == -1) {
+//         printf("client 1 accept failed\n");
+//         close(socketId);
+//         return;
+//     }
+//     printf("client 1 accepted\n");
+
+//     connId2 = accept(socketId, (struct sockaddr *)&clientAddress, &len);
+//     if (connId2 == -1) {
+//         printf("client 2 accept failed\n");
+//         close(socketId);
+//         close(connId1);
+//         return;
+//     }
+//     printf("client 2 accepted\n");
+
+//     char buffer[100];
+//     while (1) {
+//         ssize_t Received = recv(connId1, buffer, sizeof(buffer), 0);
+//         if (Received == -1) {
+//             printf("Receive from client 1 failed\n");
+//             close(socketId);
+//             close(connId1);
+//             close(connId2);
+//             return;
+//         } else if (Received == 0) {
+//             printf("client 1 disconnected\n");
+//             break;
+//         }
+//         buffer[Received] = '\0';
+//         printf("\nfrom client 1: %s\n", buffer);
+
+//         if (send(connId2, buffer, strlen(buffer) + 1, 0) == -1) {
+//             printf("send to client 2 failed\n");
+//             close(socketId);
+//             close(connId1);
+//             close(connId2);
+//             return;
+//         }
+//         printf("message sent to client 2\n");
+//     }
+    
+//     close(connId1);
+//     close(connId2);
+//     close(socketId);
+// }
